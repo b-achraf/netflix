3469fdaf2b1ea3ba21722b8e3c201e18fa2afcc6
b-achraf <projects.achraf@gmail.com>
2021-09-14 21:38:03 +0100

    Update request.js

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 const API_KEY = "f26aba8d590e51fdaca19e4fca2979fd"
 const requests = {
-    fetchNetflixOriginals: `movie/550?api_key=${API_KEY}`,
+    fetchNetflixOriginals: `movie/549?api_key=${API_KEY}`,
     trendingNow: `discover/tv?api_key=${API_KEY}&with_networks=213`
 }
 export default requests
