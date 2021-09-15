d1817e1c14fe7682f98348b1028aaf854d5cb136
b-achraf <projects.achraf@gmail.com>
2021-09-15 01:33:36 +0100

    Done Without Hosting

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import axios from "../axios";
 const baseURL = "https://image.tmdb.org/t/p/original/";
 const Row = ({ title, fetchURL, isLarge }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState("");
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClickImage = (movie) => {
+    if (trailerURL) setTrailerURL("");
+    else
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerURL(urlParams.get("v"));
+          console.log({ v: urlParams.get("v"), urlParams });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  };
   useEffect(() => {
     async function getData() {
       const request = await axios.get(fetchURL);
@@ -16,6 +39,7 @@ const Row = ({ title, fetchURL, isLarge }) => {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            onClick={() => handleClickImage(movie)}
             className={`row__poster ${isLarge ? "large__poster" : ""}`}
             key={movie.id}
             src={`${baseURL}${
@@ -25,6 +49,7 @@ const Row = ({ title, fetchURL, isLarge }) => {
           />
         ))}
       </div>
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   );
 };
